6037abc1629e257205034de59a615fa24e090086
Thinner <thinner@thinner-ThinkPad-Edge.(none)>
2012-11-02 22:04:44 +0100

    Added controller skeletons

---
+++ peg/set/controller/GameController.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package peg.set.controller;
+
+/**
+ *
+ * @author thinner
+ */
+public abstract class GameController {
+    
+}
