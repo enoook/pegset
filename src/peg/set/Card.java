42ca5f0b3055a7ba6b153ac9f1b8583ee05e4280
hello <hello@hello.com>
2012-11-02 23:04:20 +0100

    card class

--- src/peg/set/Card.java
+++ src/peg/set/Card.java
@@ -9,5 +9,36 @@ package peg.set;
  * @author HANANANANANA
  */
 public class Card {
+    public static enum SHAPE {TRIANGLE, SQUARE, CIRCLE};
+    public static enum NUMBER_OF {ONE, TWO, THREE};
+    public static enum COLOR {PINK, PURPLE, ORANGE};
+    public static enum PATTERN {BLANK, FULL, HALF};
     
+    private SHAPE shape;
+    private NUMBER_OF numberOf;
+    private COLOR color;
+    private PATTERN pattern;
+    
+    public Card(SHAPE shape, NUMBER_OF numberOf, COLOR color, PATTERN pattern) {
+    	this.shape = shape;
+    	this.numberOf = numberOf;
+    	this.color = color;
+    	this.pattern = pattern;
+    }
+    
+    public SHAPE getShape() {
+    	return shape;
+    }
+    
+    public NUMBER_OF getNumberOf() {
+    	return numberOf;
+    }
+    
+    public COLOR getColor() {
+    	return color;
+    }
+    
+    public PATTERN getPattern() {
+    	return pattern;
+    }
 }
