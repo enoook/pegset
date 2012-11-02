b2ca071eda0d1b0931cb06cce85331572f348afb
Thinner <thinner@thinner-ThinkPad-Edge.(none)>
2012-11-02 22:20:55 +0100

    Reconstruction step 3

--- gen/R.java.d
+++
@@ -1,4 +0,0 @@
-/home/thinner/NetBeansProjects/PegSet/gen/peg/set/R.java \
- : /home/thinner/NetBeansProjects/PegSet/res/layout/main.xml \
-/home/thinner/NetBeansProjects/PegSet/res/values/strings.xml \
-/home/thinner/NetBeansProjects/PegSet/bin/AndroidManifest.xml \
