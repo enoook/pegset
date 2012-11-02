e627187ed64d04dd20fdaa387a8d1c304b4a1065
Thinner <thinner@thinner-ThinkPad-Edge.(none)>
2012-11-02 23:15:32 +0100

    asd

---
+++ src/peg/set/GameBoard.java
@@ -0,0 +1,28 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package peg.set;
+
+import android.content.Context;
+import android.view.View;
+
+/**
+ *
+ * @author thinner
+ */
+public class GameBoard extends View{
+
+    public GameBoard(Context context) {
+        super(context);
+    }
+    
+    public void select(Card card) {
+        
+    }
+    
+    public void deselect(Card card) {
+        
+    }
+    
+}
